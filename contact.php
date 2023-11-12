c984696baf01545ab1e1159a39cd46aaea9d0d00
TheTerror <johnmrfaye12@gmail.com>
2023-11-12 22:34:30 +0100

    add: contact.php

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+mail("jnmrfaye12@gmail.com", "potentiel intérêt", "Hello world", "From:contact@myportfolio.com" . "\r\n" . "Reply-to:johnmrfaye12@gmail.com");
+// if (isset($_POST["message"]))
+// {
+// 	$message = "Ce message est expédié depuis le site My Portfolio.
+// 	Nom: " . $_POST["name"] . "
+// 	Email: " . $_POST["email"] . "
+// 	Message: " . $_POST["message"];
+// 	$retour = mail("jnmrfaye12@gmail.com", "potentiel intérêt", $message, "From:contact@myportfolio.com" . "\r\n" . "Reply-to:" . $_POST["email"]);
+// 	if ($retour)
+// 	{
+// 		echo("<p>expédié</p>");
+// 	}
+// }
